a205002e21787150bd8ea800ca1660e26797f902
Katrin <katrinaiko@hotmail.com>
2018-09-04 12:26:44 +0200

    follow up with routes and views

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,40 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-const Wish = require("../models/Wish");
-// const ensureLoggedIn = require("connect-ensure-login").ensureLoggedIn;
+// const Wish = require("../models/Wish");
+
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render(
-    "index"
-    // , {
-    //   isConnected: req.isAuthenticated(),
-    //   isAdmin: req.user && req.user.role === "ADMIN"
-    // }
-  );
+  res.render("index");
 });
 
-// router to users, inclusive the check for the role
-router.get("/users", checkRoles("ADMIN"), (req, res, next) => {
-  User.find().then(users => {
-    res.render(users - list, { users });
-  });
-});
 
-router.get("/wishes/new", ensureLoggedIn("/auth/login"), (req, res, next) => {
-  res.render("wishes-new");
-});
 
-router.post("/wishes/new", ensureLoggedIn("/auth/login"), (req, res, next) => {
-  console.log("DEBUG req.user", req.user);
-  Wish.create({
-    name: req.body.name,
-    description: req.body.description,
-    _owner: req.user._id //important! most of the time use this with "ensureLoggedIn"
-  }).then(newWish => {
-    res.redirect("/wishes/" + newWish._id);
-  });
-});
+// // can we use this for our search bar, in which we search for user?
+// router.get("/users", (req, res, next) => {
+//   User.find().then(users => {
+//     res.render(users - list, { users });
+//   });
+// });
+
+// later we can add ensureLoggedIn("/auth/login") here after "/wishes/new",:
+// router.get("/wishes/new", (req, res, next) => {
+//   res.render("wishes-new");
+// });
+
+// this is the route to add new wishes:
+// later we can add ensureLoggedIn("/auth/login") here after "/wishes/new",:
+// router.post("/wishes/new", (req, res, next) => {
+//   console.log("DEBUG req.user", req.user);
+//   Wish.create({
+//     name: req.body.name,
+//     description: req.body.description,
+//     _owner: req.user._id //important! most of the time use this with "ensureLoggedIn"
+//   }).then(newWish => {
+//     res.redirect("/wishes/" + newWish._id);
+//   });
+// });
 
 // can we use the function below for our price-filter?
 // router.get("/wishes/:id", (req, res, next) => {
@@ -44,7 +43,7 @@ router.post("/wishes/new", ensureLoggedIn("/auth/login"), (req, res, next) => {
 //   });
 // });
 
-// what is populate?
+// what is populate? this example is taken from another exercise:
 // router.get("/wishes", (req, res, next) => {
 //   Room.find()
 //     .populate("_owner")
@@ -52,44 +51,5 @@ router.post("/wishes/new", ensureLoggedIn("/auth/login"), (req, res, next) => {
 //       res.render("rooms", { rooms: rooms });
 //     });
 // });
-
-// do we need this?
-// Wish.findById(req.params.id).then(wish => {
-//   if (
-//     req.isAuthenticated() &&
-//     (req.user.role === "ADMIN" ||
-//       req.user._id.toString() === room._owner.toString())
-//   ) {
-//     res.render("wish-edit", { wish });
-//   } else {
-//     res.redirect("/auth/login");
-//   }
-// });
-
-// // I have no idea what this function is supposed to do
-// router.post("/rooms/:id/edit", checkRoles("ADMIN"), (req, res, next) => {
-//   Room.findById(
-//     req.params.id,
-//     {
-//       name: req.body.name,
-//       description: req.body.description
-//     },
-//     { new: true }
-//   ).then(room => {
-//     res.redirect("/rooms/" + room._id);
-//   });
-// });
-
-// do we want to check roles???
-// function checkRoles(role) {
-//   return function(req, res, next) {
-//     if (req.isAuthenticated() && req.user.role === role) {
-//       //"req." is often giving a true or false boolean
-//       return next();
-//     } else {
-//       res.redirect("/auth/login");
-//     }
-//   };
-// }
 
 module.exports = router;
