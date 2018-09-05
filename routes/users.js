ccd09e7b82e674eb1be9350de9c68d6da763d12e
Jessica Schulz <jessica.schulz3@gmx.net>
2018-09-05 15:03:18 +0200

    get Katrins Search box in navbar

--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,13 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
-router.get('/users', (req, res, next) => {
-  // User.find()
-  //   .then(usersFromDb => {
-  //     console.log("DEBUG USERS -->", users)
-  //     console.log("DEBUG usersFromDb -->", usersFromDb)
-  res.render('users')
-  // {
-  // "users": usersFromDb
-  // });
-  // })
+router.get('/', (req, res, next) => {
+  User.find()
+    .then(usersFromDb => {
+      res.render('users', {
+        "users": usersFromDb
+      });
+    })
 });
 
 // $("#username").keyup(function (event) {
