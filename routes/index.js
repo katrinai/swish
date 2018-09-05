9329e47b9d74b2897730542390854daf1be27b94
Katrin Aiko <katrinaiko@hotmail.com>
2018-09-05 13:39:40 +0200

    browse friends and search for username installed, userlist installed

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require("../models/User");
 const Wish = require("../models/Wish");
 const ensureLogin = require("connect-ensure-login");
+const mongoose = require("mongoose");
+// const users = require("/users");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -14,13 +16,7 @@ router.get("/about", (req, res, next) => {
   res.render("about");
 });
 
-// router to users
-// router.get("/users", (req, res, next) => {
-//   User.find().then(usersFromDb => {
-//     res.render("users", {"users: usersFromDb"});
-//   });
-// });
-
+//route to show the user-profile and connect to the wish list from DB
 router.get(
   "/user-profile",
   ensureLogin.ensureLoggedIn("/auth/login"),
@@ -46,5 +42,43 @@ router.post("/wishes/new", (req, res, next) => {
     res.redirect("/wishes/" + newWish._id);
   });
 });
+
+router.get(
+  "/users",
+  ensureLogin.ensureLoggedIn("/auth/login"),
+  (req, res, next) => {
+    if (req.query.search && req.query.search !== "") {
+      User.find({ username: new RegExp(req.query.search, "i") }).then(users => {
+        if (req.isAuthenticated()) {
+          res.render("users", { users });
+        } else {
+          res.redirect("/auth/login");
+        }
+      });
+    } else {
+      res.render("users", {
+        message: "Search for a user using the search bar"
+      });
+    }
+  }
+);
+
+router.get(
+  "/users/:id/wishlist",
+  ensureLogin.ensureLoggedIn("/auth/login"),
+  (req, res, next) => {
+    Wish.find({ _owner: req.params.id })
+      .populate("_owner")
+      .then(wishlists => {
+        let username = wishlists[0]._owner.username;
+        console.log("WISHKISTS ---->", wishlists);
+        if (req.isAuthenticated()) {
+          res.render("wish-list", { wishlists, username });
+        } else {
+          res.redirect("/auth/login");
+        }
+      });
+  }
+);
 
 module.exports = router;
